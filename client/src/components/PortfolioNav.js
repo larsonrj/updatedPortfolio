4c749181bf1ca598e35618cb82041a7087695aed
Ryan <rylarson17@gmail.com>
2023-02-28 18:10:51 -0700

    updated header and home page

--- client/src/components/PortfolioNav.js
+++ client/src/components/PortfolioNav.js
@@ -3,67 +3,118 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import "../components/css/nav.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 // TODO: Add a comment explaining how we are able to extract the key value pairs from props
 
 function PortfolioNav({ currentPage, handlePageChange }) {
   return (
-    <>
-      <Navbar className="p-3 navBar" variant="dark">
-        <Container>
-          <Nav>
-            <Nav.Link
-              href="#home"
-              onClick={() => handlePageChange("Home")}
-              className={
-                currentPage === "Home" ? "nav-link active" : "nav-link"
-              }
-              id="home"
-            >
-              Ryan Larson
-            </Nav.Link>
-          </Nav>
-          <Nav className="justify-content-end link">
-            <Nav.Link
-              href="#about"
-              onClick={() => handlePageChange("About")}
-              className={
-                currentPage === "About" ? "nav-link active" : "nav-link"
-              }
-            >
-              About Me
-            </Nav.Link>
-            <Nav.Link
-              href="#projects"
-              onClick={() => handlePageChange("Projects")}
-              className={
-                currentPage === "Projects" ? "nav-link active" : "nav-link"
-              }
-            >
-              Projects
-            </Nav.Link>
-            <Nav.Link
-              href="#contact"
-              onClick={() => handlePageChange("Contact")}
-              className={
-                currentPage === "Contact" ? "nav-link active" : "nav-link"
-              }
-            >
-              Contact
-            </Nav.Link>
-            <Nav.Link
-              href="#resume"
-              onClick={() => handlePageChange("Resume")}
-              className={
-                currentPage === "Resume" ? "nav-link active" : "nav-link"
-              }
-            >
-              Resume
-            </Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-    </>
+    <div className="navBar d-flex justify-content-center">
+      <div className="d-flex col-8 justify-content-center">
+        <div className="col-6 align-self-center text-center">
+          <a
+            href="#home"
+            className={
+              currentPage === "Home"
+                ? "home align-self-center fa fa-dribbble"
+                : "home align-self-center fa fa-dribbble"
+            }
+            onClick={() => handlePageChange("Home")}
+          ></a>
+        </div>
+        <div className="d-flex col-6 justify-content-end text-end">
+          <a
+            href="#about"
+            className={
+              currentPage === "About"
+                ? "active-link col-4 align-self-center"
+                : "nav-links col-4 align-self-center"
+            }
+            onClick={() => handlePageChange("About")}
+          >
+            About Me
+          </a>
+          <a
+            href="#projects"
+            className={
+              currentPage === "Projects"
+                ? "active-link col-4 align-self-center"
+                : "nav-links col-4 align-self-center"
+            }
+            onClick={() => handlePageChange("Projects")}
+          >
+            Projects
+          </a>
+          <a
+            href="#resume"
+            className={
+              currentPage === "Resume"
+                ? "active-link col-4 align-self-center"
+                : "nav-links col-4 align-self-center"
+            }
+            onClick={() => handlePageChange("Resume")}
+          >
+            Resume
+          </a>
+        </div>
+      </div>
+    </div>
+    // <>
+    //   <Navbar className="p-3 navBar" variant="dark">
+    //     <Container>
+    //       <Nav>
+    //         <Nav.Link
+    //           href="#home"
+    //           onClick={() => handlePageChange("Home")}
+    //           className={
+    //             currentPage === "Home" ? "nav-link active" : "nav-link"
+    //           }
+    //           id="home"
+    //         >
+    //           <i className="fas fa-basketball-ball"></i>
+    //         </Nav.Link>
+    //       </Nav>
+    //       <Nav className="justify-content-end link">
+    //         <Nav.Link
+    //           href="#about"
+    //           onClick={() => handlePageChange("About")}
+    //           className={
+    //             currentPage === "About" ? "nav-link active" : "nav-link"
+    //           }
+    //         >
+    //           About Me
+    //         </Nav.Link>
+    //         <Nav.Link
+    //           href="#projects"
+    //           onClick={() => handlePageChange("Projects")}
+    //           className={
+    //             currentPage === "Projects" ? "nav-link active" : "nav-link"
+    //           }
+    //         >
+    //           Projects
+    //         </Nav.Link>
+    //         <Nav.Link
+    //           href="#contact"
+    //           onClick={() => handlePageChange("Contact")}
+    //           className={
+    //             currentPage === "Contact" ? "nav-link active" : "nav-link"
+    //           }
+    //         >
+    //           Contact
+    //         </Nav.Link>
+    //         <Nav.Link
+    //           href="#resume"
+    //           onClick={() => handlePageChange("Resume")}
+    //           className={
+    //             currentPage === "Resume" ? "nav-link active" : "nav-link"
+    //           }
+    //         >
+    //           Resume
+    //         </Nav.Link>
+    //       </Nav>
+    //     </Container>
+    //   </Navbar>
+    // </>
   );
 }
 
--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,10 +1,23 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "../css/home.css";
+import prof from "../images/rlarson.PNG";
 
 export default function Home() {
   return (
     <div className="flex-grow-1 d-flex justify-content-center">
-      <div className="rounded-5 col-8 h-75 bg-dark align-self-center"></div>
+      <div className="d-flex justify-content-evenly rounded-5 col-8 h-75 home align-self-center">
+        <div className="h-75 col-4 align-self-center title d-flex flex-column">
+          <div>Ryan Larson</div>
+          <div>Full Stack Developer</div>
+        </div>
+        <div className="d-flex col-4 h-75 align-self-center">
+          <img
+            className="rounded-5 align-self-center h-100 w-100"
+            src={prof}
+          ></img>
+        </div>
+      </div>
     </div>
   );
 }
